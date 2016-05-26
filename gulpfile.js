126960c33e288d2e97d0d272535aab6827da15fd
Jesse Allen <jesse@jessecarl.com>
2016-05-26 10:43:33 -0700

    Setup Gulp task to import and build sass
    Sets up Gulp task to import sass dependencies and build from main.scss.
    The path may need to be updated in the future if additional style sheets
    are wanted.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,32 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    fontAwesome = require('node-font-awesome'),
+    bourbon = require('node-bourbon'),
+    neat = require('bourbon-neat');
 
-gulp.task('default', function(){
+var paths = {
+  sass: "./sass/main.scss" // everything is from main
+};
+
+gulp.task('default', ['sass','fonts']);
+
+gulp.task('sass', function(){
+  return gulp.src(paths.sass)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: [
+        require('node-normalize-scss').includePaths,
+        fontAwesome.scssPath,
+        bourbon.includePaths,
+        neat.includePaths
+      ]
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./static/css'));
+});
+
+gulp.task('fonts', function() {
+    gulp.src(fontAwesome.fonts)
+      .pipe(gulp.dest('./static/fonts'));
 });
