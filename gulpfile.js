8489f29458766c48c1ca069182b22cc6dd95a497
Jesse Allen <jesse@jessecarl.com>
2016-05-27 17:15:04 -0700

    Remove Gulp Responsive Image Generation
    Removes Gulp Script for generating responsive images.
    We will be using a quick go application instead.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,30 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
+    clean = require('gulp-clean'),
     fontAwesome = require('node-font-awesome'),
     bourbon = require('node-bourbon'),
     neat = require('bourbon-neat');
 
 var paths = {
-  sass: ["./sass/**/*.scss","!./sass/**/_*.scss"] // do not directly compile scss files that begin with "_"
+  sass: ["./sass/**/*.scss","!./sass/**/_*.scss"], // do not directly compile scss files that begin with "_"
+  photos: "./photos/**/*.jpg"
+};
+
+var outputDirs = {
+  css: './static/g/css',
+  fonts: './static/g/fonts',
+  photos: './static/s/img/photo'
 };
 
 gulp.task('default', ['sass','fonts']);
 
-gulp.task('sass', function(){
+gulp.task('clean-css', function() {
+  return gulp.src(outputDirs.css, {read: false})
+    .pipe(clean());
+});
+
+gulp.task('sass', ['clean-css'], function(){
   return gulp.src(paths.sass)
     .pipe(sourcemaps.init())
     .pipe(sass({
@@ -23,12 +36,18 @@ gulp.task('sass', function(){
       ]
     }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./static/g/css/'));
+    .pipe(gulp.dest(outputDirs.css));
 });
 
-gulp.task('fonts', function() {
-    gulp.src(fontAwesome.fonts)
-      .pipe(gulp.dest('./static/g/fonts/'));
+gulp.task('clean-fonts', function() {
+  return gulp.src(outputDirs.fonts, {read: false})
+    .pipe(clean());
+});
+
+gulp.task('fonts', ['clean-fonts'], function() {
+  var stream = gulp.src(fontAwesome.fonts)
+    .pipe(gulp.dest(outputDirs.fonts));
+  return stream // make the fonts task run after dependencies run
 });
 
 var logChanges = function(event) {
@@ -36,5 +55,5 @@ var logChanges = function(event) {
 };
 
 gulp.task('watch', ['default'], function() {
-  gulp.watch('./sass/**/*.scss', ['sass']).on('change', logChanges);
+  gulp.watch(paths.sass[0], ['sass']).on('change', logChanges);
 });
